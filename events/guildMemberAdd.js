8c3d708d1228baabee7eecbb62815cdf1097e7ca
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-02-02 00:16:21 +0330

    GuildMemberAdd event added

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,29 @@
+const config = require("../config.json")
+const {MessageEmbed} = require("discord.js")
+
+module.exports = {
+    name : "guildMemberAdd",
+    description : "Handle when a member joined to server",
+    execute(client, member){
+
+        const welcomeChannel = client.channels.cache.get(config.channels.welcome)
+        if(welcomeChannel){
+            const welcomeEmbed = new MessageEmbed()
+            .setColor(config.embeds.color)
+            .setThumbnail(member.user.displayAvatarURL({dynamic : true, size : 1024}))
+            .setAuthor({name : `Welcome ${member.user.username}`})
+            .setDescription(`Hey ${member.user.username} ! Khosh oomadi be server ${member.guild.name}.`)
+            .setFooter({text : `${member.guild.name} Server`, iconURL : member.guild.iconURL()})
+            welcomeChannel.send({embeds : [welcomeEmbed]})
+        }
+
+        const welcomeDMEmbed = new MessageEmbed()
+        .setColor(config.embeds.color)
+        .setAuthor({name : `خوش اومدی به ${member.guild.name}`, iconURL : member.user.displayAvatarURL({dynamic : true})})
+        .setDescription(`سلام ${member.user.username}. خوش اومدی به سرور ${member.guild.name} !\n\nامیدوارم لحظات خوبی رو توی سرور داشته باشی\n\nبهت پیشنهاد میکنم به چت اصلی سرور سر بزنی <#${config.channels.generalChat}>`)
+        .setThumbnail(member.guild.iconURL({dynamic : true, size : 1024}))
+        .setFooter({text : `${member.guild.name} Server`, iconURL : member.guild.iconURL()})
+        member.send({embeds : [welcomeDMEmbed]})
+    
+    }
+}
