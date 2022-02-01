3bf517f70fc74c96c283bbebf36b0aaebf0053fa
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-02-02 02:51:22 +0330

    non-slash command supported

---
+++ events/messageCreate.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+const config = require("../config.json")
+
+module.exports = {
+    name : "messageCreate",
+    description : "Handle when a user sent message",
+    execute(client, message){
+        const messageArry = message.content.split(" ")
+        const cmd = messageArry[0].replace(config.bot.prefix, "")
+
+        if(!client.commands.has(cmd) || !client.commands.get(cmd).executeCommand) return;
+
+        const grabbedCommand = client.commands.get(cmd)
+        if(!grabbedCommand.permissions && !grabbedCommand.private) grabbedCommand.executeCommand(client, message);
+
+    }
+}
