7dff0141915e276fa18d315205ffdeed3e25c48d
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-02-07 14:06:56 +0330

    setChannel fixed

--- commands/setchannel.js
+++ commands/setchannel.js
@@ -1,17 +1,18 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
+const config = require("../config.json")
 
 module.exports = {
     name : "setchannel",
     permissions : ["ADMINISTRATOR"],
-    everyoneCanUse = false,
+    everyoneCanUse : false,
 
     data : new SlashCommandBuilder()
         .setName("setchannel")
         .setDescription("Update channel for logs")
         .addStringOption(option =>
-            option.setName("name")
-            .setDescription("Enter name for channel")
+            option.setName("event")
+            .setDescription("Enter event name")
             .setRequired(true)
             .addChoice("Channel Create", "channelCreate")
             .addChoice("Channel Delete", "channelDelete")
@@ -23,12 +24,44 @@ module.exports = {
 
         .addStringOption(option => 
             option.setName("channel")
-            .setDescription("Enter channel id or mention it")
+            .setDescription("Enter channel id or channel name")
             .setRequired(true)
         ),
 
     async execute(client, interaction) {
-        console.log(interaction.getString("name"))
+        const event = await interaction.options.getString("event")
+        let channel = await interaction.options.getString("channel")
+
+        const embed = new MessageEmbed()
+        .setColor(config.colors.main)
+        .setFooter({text : `${interaction.guild.name} Server`, iconURL : interaction.guild.iconURL()})
+        .setAuthor({name : "Logs", iconURL : interaction.guild.iconURL()})
+
+        if(!channel){
+            embed.setDescription(`Channel for **${event}** is not set or channel not found`)
+            return await interaction.reply({embeds : [embed], ephemeral : true})
+        }
+
+        let logChannel = await client.data.logChannel(event)
+        if(logChannel) {
+            const findChannel = interaction.guild.channels.cache.get(channel)
+            if(findChannel){
+
+                logChannel.channelId = channel
+                logChannel.save().then(async () => {
+                    embed.setDescription(`Channel for **${event}** is set to **${channel}**`)
+                    await interaction.reply({embeds : [embed], ephemeral : true})
+                })
+
+            }else{
+                embed.setDescription(`Channel for **${event}** is not set or channel not found`)
+                await interaction.reply({embeds : [embed], ephemeral : true})
+            }
+        }else{
+            embed.setDescription(`Channel for **${event}** is not set or channel not found`)
+            await interaction.reply({embeds : [embed], ephemeral : true})
+        }
+
     },
 
     async executeCommand(client, message) {
