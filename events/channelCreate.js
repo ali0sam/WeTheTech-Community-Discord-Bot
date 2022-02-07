63df50db6e6075ae2da7509a1f0cd2a10e9f15a2
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-02-07 14:07:11 +0330

    channelCreate event fixed and conntected to database

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -5,23 +5,29 @@ module.exports = {
     name : "channelCreate",
     description : "Handle when a channel created",
     async execute(client, channel){
-        const findChannel = client.channels.cache.get(config.channels.logs.channel)
-        if(!findChannel) return;
+        const logChannelId = await client.data.logChannel(this.name)
+        if(logChannelId && logChannelId.channelId){
 
-        let Entry = NaN;
-        try {
-          const AuditLogFetch = await newMember.guild.fetchAuditLogs({
-            limit: 1,
-            type: "CHANNEL_CREATE",
-          });
-          Entry = AuditLogFetch.entries.first();
-        } catch {}
+          const findChannel = client.channels.cache.get(logChannelId.channelId)
+          if(!findChannel) return;
 
-        const logEmbed = new MessageEmbed()
-        .setColor(config.colors.main)
-        .setFooter({text : `${channel.guild.name} Server`})
-        .setAuthor({name : "Log | Channel Create"})
-        .setDescription(`Channel ${channel} [ ${channel.name} ] created by ${Entry.executor || undefined} [ ${Entry.executor.id || undefined} ]`)
-        findChannel.send({embeds : [logEmbed]})
+          let Entry = NaN;
+          try {
+            const AuditLogFetch = await channel.guild.fetchAuditLogs({
+              limit: 1,
+              type: "CHANNEL_CREATE",
+            });
+            Entry = AuditLogFetch.entries.first();
+          } catch (e){console.log(e)}
+
+          const logEmbed = new MessageEmbed()
+          .setColor(config.colors.main)
+          .setFooter({text : `${channel.guild.name} Server`})
+          .setAuthor({name : "Log | Channel Create"})
+          .setDescription(`Channel ${channel} [ ${channel.name} ] created by ${Entry.executor || undefined}`)
+          findChannel.send({embeds : [logEmbed]})
+
+        }
+
     }
 }
